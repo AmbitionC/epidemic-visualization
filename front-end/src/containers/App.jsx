77908dd16129f25c0d839398e8475536fc38407d
ashinzhang <624154230@qq.com>
2020-02-14 23:00:47 +0800

    菜单收缩功能实现，菜单切换功能实现

--- front-end/src/containers/App.jsx
+++ front-end/src/containers/App.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 
 import { Layout } from 'antd';
-import Nav from './Nav';
+import Main from './Main';
 
 import 'css/normalize.css';
 import 'css/main.css';
 
-const { Header, Sider, Content } = Layout;
+const { Header } = Layout;
 
 export default () => {
   return (
     <Layout>
       <Header>疫情数据可视化平台</Header>
       <Layout className='main-wrapper'>
-        <Sider>
-          <Nav />
-        </Sider>
-        <Content>Content</Content>
+        <Main />
       </Layout>
     </Layout>
   );
---
+++ front-end/src/containers/Main.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useMemo } from 'react';
+
+import { Layout, Menu, Icon, Button } from 'antd';
+
+const { Sider, Content } = Layout;
+
+// 这两个组件仅作为测试用
+const TestComponent0 = () => <div>TestComponent0</div>
+const TestComponent1 = () => <div>TestComponent1</div>
+
+// 导航菜单的内容，主要包括：图标、名称、点击之后对应的主区域的组件
+const menu = [
+  {
+    icon: 'dot-chart',
+    name: '测试页面',
+    component: <TestComponent0 />,
+  },
+  {
+    icon: 'line-chart',
+    name: '测试组件',
+    component: <TestComponent1 />,
+  },
+];
+
+export default () => {
+  // 菜单是否收起
+  const [collapsed, setCollapsed] = useState(false);
+  // 菜单的索引，和上面的menu相同
+  const [componentIndex, setComponentIndex] = useState(0);
+  // 使用memo使得点击菜单时，不会重复刷新菜单
+  const navigation = useMemo(() => {
+    // 菜单点击之后的触发的函数
+    const handleClick = e => setComponentIndex(e.key);
+    return (
+      <Sider collapsed={collapsed}>
+        <Menu
+          defaultSelectedKeys={['0']}
+          mode="inline"
+          theme="light"
+          onClick={handleClick}
+        >
+          {menu.map((item, index) => {
+            return (
+              <Menu.Item key={index}>
+                <Icon type={item.icon} />
+                <span>{item.name}</span>
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+        <Button onClick={() => setCollapsed(!collapsed)}>
+          <Icon type={collapsed ? 'right' : 'left'} />
+        </Button>
+      </Sider>
+    );
+  }, [collapsed]);
+
+  return (
+    <>
+      {navigation}
+      <Content>
+        {menu[componentIndex]['component']}
+      </Content>
+    </>
+  );
+}
