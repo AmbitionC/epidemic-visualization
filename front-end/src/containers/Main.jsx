d7ee02f64e3a7e868a9b6cc99a1d542579b0901e
itsMLiuJunLe <48476754+itsMLiuJunLe@users.noreply.github.com>
2020-02-15 09:38:19 +0800

    Add files via upload
    添加了form表单

--- front-end/src/containers/Main.jsx
+++ front-end/src/containers/Main.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useMemo } from 'react';
 
 import { Layout, Menu, Icon, Button } from 'antd';
-
+import DonateForm from '../form/DonateForm'
+import NeedForm from '../form/NeedForm'
 const { Sider, Content } = Layout;
 
 // 这两个组件仅作为测试用
 const TestComponent0 = () => <div>TestComponent0</div>
 const TestComponent1 = () => <div>TestComponent1</div>
+//
 
 // 导航菜单的内容，主要包括：图标、名称、点击之后对应的主区域的组件
 const menu = [
@@ -20,6 +22,16 @@ const menu = [
     name: '测试组件',
     component: <TestComponent1 />,
   },
+  {
+    icon: 'team',
+    name: '提供捐赠',
+    component: <DonateForm />,
+  },
+  {
+    icon: 'team',
+    name: '获取需求',
+    component: <NeedForm />,
+  },
 ];
 
 export default () => {
@@ -30,7 +42,7 @@ export default () => {
   // 使用memo使得点击菜单时，不会重复刷新菜单
   const navigation = useMemo(() => {
     // 菜单点击之后的触发的函数
-    const handleClick = e => setComponentIndex(e.key);
+  const handleClick = e => setComponentIndex(e.key);
     return (
       <Sider collapsed={collapsed}>
         <Menu
