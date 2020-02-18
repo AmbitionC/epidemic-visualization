835f5b51e511cc1a82d19f88cba40e132284cb2f
itsMLiuJunLe <liujunle940813@163.com>
2020-02-18 10:17:14 +0800

    增加了顶部导航与路由

--- front-end/src/containers/App.jsx
+++ front-end/src/containers/App.jsx
@@ -1,35 +1,52 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import { Layout } from 'antd';
+import { Layout, Button } from 'antd';
 import Head from './Head';
 import Map from 'components/map/Map';
+import DonateForm from 'components/form/DonateForm';
+import NeedForm from 'components/form/NeedForm';
 
 import 'css/normalize.css';
 import 'css/main.css';
 
-const T = () => <div style={{ width: '100%', height: '100%', background: '#dddddd' }}>组件区域</div>
+export const T1 = () => <div style={{ width: '100%', height: '100%', background: '#dddddd' }}>组件区域</div>
+export const T2= () => <div style={{ width: '100%', height: '100%', background: '#d1d1d1' }}>组件区域2</div>
 
 const { Header } = Layout;
 
 export default () => {
   return (
-    <Layout>
-      <Header>
-        <Head />
-      </Header>
-      <Layout className='main-wrapper'>
-        <div className="col">
-          <T />
-        </div>
-        <div className="col">
-          <div className="row" style={{ height: '66.67%' }}>
-            <Map />
+    <Router>
+      <Layout>
+        <Header>
+          <Head />
+        </Header>
+        <Layout className='main-wrapper'>
+          <Route path="/platform/">
+            <div className="col">
+              <Route path="/platform/donateform" component={DonateForm}/>
+              <Route path="/platform/needform" component={NeedForm}/>
+            </div>
+            <div className="col" >
+              <div className="row" style={{ height: '66.67%' }}>
+                <Map />
+              </div>
+              <div className="row" style={{ height: '33.33%' }}>
+                <T2/>
+              </div>
+            </div>
+          </Route>
+          <Route path="/" exact>
+          <div className="home-page" >
+            {/*<Button type="primary" style={{float:'left',zIndex:'100',marginTop:'15px',marginLeft:'15px'}}><Link to="/platform/">进入平台</Link></Button>*/}
+            <div className="row" style={{ height: '100%' }}>
+              <Map />
+            </div>
           </div>
-          <div className="row" style={{ height: '33.33%' }}>
-            <T />
-          </div>
-        </div>
+          </Route>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 }
--- front-end/src/containers/Head.jsx
+++ front-end/src/containers/Head.jsx
@@ -1,8 +1,56 @@
 import React from 'react';
 import { variableMap } from 'global/variable'
+import { Menu, Icon } from 'antd';
+import { Link} from 'react-router-dom';
+import Search from '../components/search/Search';
+
+const menuItem = [
+  {
+    icon: 'book',
+    key: '首页',
+    name: variableMap.homePage,
+    routeurl: '/'
+  },
+  {
+    icon: 'usergroup-add',
+    key: 'donate',
+    name: variableMap.menuDonate,
+    routeurl: '/platform/donateform/'
+    
+  },
+  {
+    icon: 'usergroup-delete',
+    key: 'need',
+    name: variableMap.menuNeed,
+    routeurl: '/platform/needform/'
+  },
+  {
+    icon: 'link',
+    key: 'test',
+    name: variableMap.test,
+    routeurl: '/platform/test/'
+  }
+];
+
 
 export default () => {
+  const [current, setCurrent] = useState('首页')
   return (
-    <div>{variableMap.header}</div>
+    <Fragment>
+      <div style={{float:'left',height:'48px',lineHeight:'48px'}}>{variableMap.header}</div>
+      <div style={{float:'left',marginLeft:'20px'}}>
+        <Menu onClick={e => setCurrent(e.key)} selectedKeys={current} mode="horizontal">
+          {menuItem.map((item) => {
+            //console.log(item)
+            return (
+              <Menu.Item key={item.key}>
+                <Link to={item.routeurl}><Icon type={item.icon} />{item.name}</Link>
+              </Menu.Item>
+            );
+          })}
+        </Menu>
+      </div>
+      <div style={{float:'right',marginRight:'20px'}}><Search /></div>
+    </Fragment>
   );
 }
