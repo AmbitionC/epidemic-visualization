072aa51d0329138c0a05154a9dd37140ce46d899
itsMLiuJunLe <liujunle940813@163.com>
2020-02-25 12:40:48 +0800

    增加了地图标注信息

---
+++ front-end/src/components/map/pointLayer.jsx
@@ -0,0 +1,34 @@
+import { Popup, Marker, MarkerLayer } from '@antv/l7';
+
+import { urlMap, requestHospital } from 'global/url';
+
+export const addHospital = scene => {
+  scene.addImage(
+    'marker',
+    urlMap.mapIcon
+  );
+  requestHospital().then(data => {
+    const colors = [ '#00BFFF', '#3CB371', '#FFA500', '#FFDEAD', '#98FB98'];
+    const markerLayer = new MarkerLayer();
+    for (let i = 0; i < data.length; i++){
+      const popup = new Popup({
+      })
+        .setText(data[i].hospitalName);
+      const el = document.createElement('hospital');
+      el.className = 'hospitallabel';
+      el.style.background = colors[i%5];
+      el.style.borderColor = colors[i%5];
+      el.style.width = '18px';
+      el.style.height = '18px';
+      el.style.borderRadius = '9px';
+      const marker = new Marker({
+        element: el
+      }).setLnglat({ lng: data[i].longitude * 1, lat: data[i].latitude})
+        .setPopup(popup);
+
+      markerLayer.addMarker(marker);
+    }
+
+    scene.addMarkerLayer(markerLayer);
+  })
+}
