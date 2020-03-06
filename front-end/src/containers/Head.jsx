37fc8e08a4c0e8d62f358fb7eb937d9c78d6e537
itsMLiuJunLe <liujunle940813@163.com>
2020-03-06 10:23:20 +0800

    容器组件

--- front-end/src/containers/Head.jsx
+++ front-end/src/containers/Head.jsx
@@ -5,7 +5,8 @@ import { Link} from 'react-router-dom';
 import SearchBox from '../components/search/Search';
 import DonateFormDrawer from '../components/formdrawer/DonateFormDrawer';
 import NeedFormDrawer from '../components/formdrawer/NeedFormDrawer';
-import StatisticDrawer from '../components/formdrawer/StatisticDrawer';
+import StatisticDrawer from '../components/statistic/StatisticDrawer';
+import { StatisticData } from '../components/statistic/statistic_data';
 
 const menuItem = [
   {
@@ -57,7 +58,7 @@ export const Head = () => {
       </div>
       <div style={{float:'left',marginLeft:'20px',height:'48px',lineHeight:'48px'}}><DonateFormDrawer /></div>
       <div style={{float:'left',marginLeft:'20px',height:'48px',lineHeight:'48px'}}><NeedFormDrawer /></div>
-      <div style={{float:'left',marginLeft:'20px',height:'48px',lineHeight:'48px'}}><StatisticDrawer /></div>
+      <div style={{float:'left',marginLeft:'20px',height:'48px',lineHeight:'48px'}}><StatisticData><StatisticDrawer /></StatisticData></div>
       <div style={{float:'right',marginRight:'20px'}}>
         <CountContext.Provider value={{pot,setPot}}>  
           <SearchBox />
