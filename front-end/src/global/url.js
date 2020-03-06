3d978e9909664f42c357f29fc05a278283d00217
itsMLiuJunLe <liujunle940813@163.com>
2020-03-06 10:25:50 +0800

    增加日期获取

--- front-end/src/global/url.js
+++ front-end/src/global/url.js
@@ -17,7 +17,22 @@ export async function requestHospital() {
 } 
 
 export async function requestCity() {
-  const res = await Axios.get('https://lab.isaaclin.cn'+'/nCoV/api/area?latest=1&province=湖北省');
+  const res = await Axios.get('https://lab.isaaclin.cn' + '/nCoV/api/area?latest=1&province=湖北省');
   return res.data.results[0].cities[0];
 }
 
+export function getNowFormatDate() {
+  var date = new Date();
+  var seperator1 = "-";
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var strDate = date.getDate();
+  if (month >= 1 && month <= 9) {
+      month = "0" + month;
+  }
+  if (strDate >= 0 && strDate <= 9) {
+      strDate = "0" + strDate;
+  }
+  var currentdate = year + seperator1 + month + seperator1 + strDate;
+  return currentdate;
+}
