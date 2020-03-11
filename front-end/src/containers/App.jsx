091a6ac8b0be2685c4c61916adb286ee94734e1f
itsMLiuJunLe <liujunle940813@163.com>
2020-03-11 15:23:45 +0800

    改了个waring

--- front-end/src/containers/App.jsx
+++ front-end/src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import { Layout } from 'antd';
